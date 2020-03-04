18e0797987b71c029767ff505b01de5e74d388c3
Craig McMahon <mcmahon.craig@gmail.com>
2020-03-03 20:02:05 -0500

    Started autoconvert script.

--- convertandcommit.bat
+++ convertandcommit.bat
@@ -1,1 +1,19 @@
-jupyter nbconvert "L14 - Inheritance and Polymorphism.ipynb" --TagRemovePreprocessor.enabled=True --to pdf --output-dir pdf --TagRemovePreprocessor.remove_cell_tags="['nbconvert-hide']"
+:: Name: convertandcommit.bat
+:: Converts iPython Notebooks to PDF and commits them with git
+:: Author: turbonoodles
+
+REM Filename of the notebook we are operating on should be the first and only argument
+SET filename = %1 
+
+REM Convert notebook to pdf, ignoring cells tagged "nbconvert-hide"
+
+start jupyter nbconvert notebook ^
+--TagRemovePreprocessor.enabled=True ^
+--to pdf ^
+--output-dir pdf ^
+--TagRemovePreprocessor.remove_cell_tags="['nbconvert-hide']"
+
+REM Stage all changes and commit
+
+start git stage .
+REM start git commit
