4754bcfc55150d42ee7f4a4075cd28203ef21063
Craig McMahon <mcmahon.craig@gmail.com>
2020-03-05 17:58:12 -0500

    Added L15 notes.

--- convertandcommit.bat
+++ convertandcommit.bat
@@ -2,18 +2,30 @@
 :: Converts iPython Notebooks to PDF and commits them with git
 :: Author: turbonoodles
 
-REM Filename of the notebook we are operating on should be the first and only argument
-SET filename = %1 
+@ECHO off
 
+:START
+
+REM Filename of the notebook we are operating on should be the first and only argument
+SET notebook=%1
+ECHO Converting:
+ECHO %notebook%
+pause
+
+:CONVERT
 REM Convert notebook to pdf, ignoring cells tagged "nbconvert-hide"
 
-start jupyter nbconvert notebook ^
+START jupyter nbconvert %notebook% ^
 --TagRemovePreprocessor.enabled=True ^
 --to pdf ^
 --output-dir pdf ^
 --TagRemovePreprocessor.remove_cell_tags="['nbconvert-hide']"
 
+:GIT
+
 REM Stage all changes and commit
 
-start git stage .
+START git stage .
 REM start git commit
+
+:END
